0bd6538acd964265c9d4d27dbf84619ae8b56972
lukaszkaminski <lukasz.kaminski@ochk.pl>
2022-06-18 13:16:23 +0200

    Improve style of create game form

--- frontend/src/components/CreateGameForm/CreateGameForm.tsx
+++ frontend/src/components/CreateGameForm/CreateGameForm.tsx
@@ -15,6 +15,7 @@ import {
   Input
 } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
+import { BiPlus } from "react-icons/bi";
 
 export const CreateGameForm: FC = () => {
   const { errors, isSubmitting } = useFormikContext<CreateGameFormValues>();
@@ -24,40 +25,50 @@ export const CreateGameForm: FC = () => {
 
   return (
     <Form>
-      <HStack spacing="2">
-        <Field name={CreateGameFormFields.HOME_NAME}>
-          {(props: FieldProps<string>) => (
-            <FormControl isInvalid={!!errors[CreateGameFormFields.HOME_NAME]} height="28">
-              <FormLabel
-                htmlFor={CreateGameFormFields.HOME_NAME}>
-                {t('createGameForm.homeName')}
-              </FormLabel>
-              <Input id={CreateGameFormFields.HOME_NAME} type="text" {...props.field} />
-              <FormErrorMessage>
-                {translateError(errors[CreateGameFormFields.HOME_NAME])}
-              </FormErrorMessage>
-            </FormControl>
-          )}
-        </Field>
-        <Field name={CreateGameFormFields.AWAY_NAME}>
-          {(props: FieldProps<string>) => (
-            <FormControl isInvalid={!!errors[CreateGameFormFields.AWAY_NAME]} height="28">
-              <FormLabel htmlFor={CreateGameFormFields.AWAY_NAME}>
-                {t('createGameForm.awayName')}
-              </FormLabel>
-              <Input id={CreateGameFormFields.AWAY_NAME} type="text" {...props.field} />
-              <FormErrorMessage>
-                {translateError(errors[CreateGameFormFields.AWAY_NAME])}
-              </FormErrorMessage>
-            </FormControl>
-          )}
-        </Field>
-        <Box>
-          <Button type="submit" isLoading={isSubmitting} isDisabled={isSubmitting} mb="2">
-            {t('common.add')}
-          </Button>
-        </Box>
-      </HStack>
+      <Box
+        width="full"
+        borderRadius="8px"
+        borderColor="gray.300"
+        borderWidth="1px"
+        padding="4"
+        marginBottom="8"
+      >
+        <HStack spacing="2">
+          <Field name={CreateGameFormFields.HOME_NAME}>
+            {(props: FieldProps<string>) => (
+              <FormControl isInvalid={!!errors[CreateGameFormFields.HOME_NAME]} height="28">
+                <FormLabel
+                  htmlFor={CreateGameFormFields.HOME_NAME}>
+                  {t('createGameForm.homeName')}
+                </FormLabel>
+                <Input id={CreateGameFormFields.HOME_NAME} type="text" {...props.field} />
+                <FormErrorMessage>
+                  {translateError(errors[CreateGameFormFields.HOME_NAME])}
+                </FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Field name={CreateGameFormFields.AWAY_NAME}>
+            {(props: FieldProps<string>) => (
+              <FormControl isInvalid={!!errors[CreateGameFormFields.AWAY_NAME]} height="28">
+                <FormLabel htmlFor={CreateGameFormFields.AWAY_NAME}>
+                  {t('createGameForm.awayName')}
+                </FormLabel>
+                <Input id={CreateGameFormFields.AWAY_NAME} type="text" {...props.field} />
+                <FormErrorMessage>
+                  {translateError(errors[CreateGameFormFields.AWAY_NAME])}
+                </FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Box>
+            <Button type="submit" isLoading={isSubmitting} isDisabled={isSubmitting}
+              rightIcon={<BiPlus/>} mb="2">
+              {t('common.add')}
+            </Button>
+          </Box>
+        </HStack>
+      </Box>
     </Form>
   );
 }
